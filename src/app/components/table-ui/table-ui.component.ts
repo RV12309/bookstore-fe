ad8b294ce9877344bac3da4f10a4632768d2b0f2
Duy Le Quang <duyle7581@gmail.com>
2023-11-04 17:26:22 +0700

    ud base table, filter, page-header

--- src/app/components/table-ui/table-ui.component.ts
+++ src/app/components/table-ui/table-ui.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-table-ui',
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./table-ui.component.scss']
 })
 export class TableUiComponent {
-
+  @Input() headerTable = '';
+  @Input() total = 0;
+  @Input() isRefresh = true;
+  public products = [
+    {
+      id: '1000',
+      code: 'f230fh0g3',
+      name: 'Bamboo Watch',
+      description: 'Product Description',
+      image: 'bamboo-watch.jpg',
+      price: 65,
+      category: 'Accessories',
+      quantity: 24,
+      inventoryStatus: 'INSTOCK',
+      rating: 5
+    },
+    {
+      id: '1000',
+      code: 'f230fh0g3',
+      name: 'Bamboo Watch',
+      description: 'Product Description',
+      image: 'bamboo-watch.jpg',
+      price: 65,
+      category: 'Accessories',
+      quantity: 24,
+      inventoryStatus: 'INSTOCK',
+      rating: 5
+    }
+  ]
 }
--- src/app/components/table-ui/table-ui.module.ts
+++ src/app/components/table-ui/table-ui.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableUiComponent } from './table-ui.component';
-
-
+import { TableModule } from 'primeng/table';
+import { CardModule } from "primeng/card";
+import { ButtonModule } from "primeng/button";
+import { TooltipModule } from 'primeng/tooltip';
 
 @NgModule({
   declarations: [
     TableUiComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    TableModule,
+    CardModule,
+    ButtonModule,
+    TooltipModule
   ],
   exports: [
     TableUiComponent
